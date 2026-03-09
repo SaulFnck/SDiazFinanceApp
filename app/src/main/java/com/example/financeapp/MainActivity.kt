6bc4d7247c87919b613d3cf9b08487583a4a079f
SaulFnck <sauld5354@gmail.com>
2026-03-08 18:21:33 -0600

    Welcome interface

--- app/src/main/java/com/example/financeapp/MainActivity.kt
+++ app/src/main/java/com/example/financeapp/MainActivity.kt
@@ -5,20 +5,36 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.financeapp.ui.theme.FinanceAppTheme
+import com.example.financeapp.ui.theme.beish
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -44,7 +60,64 @@ fun FinanceApp(innerPadding: PaddingValues){
     ) {
 
         //Welcome
-        Row() { }
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(15.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween
+
+        )
+        {
+            //Usuario
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+            )
+            {
+                //Icono
+                Box(
+                    modifier = Modifier
+                        .size(60.dp)
+                        .clip(CircleShape)
+                        .background(beish),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.AccountCircle,
+                        contentDescription = "Profile Picture",
+                        modifier = Modifier.size(50.dp),
+                        tint = Color.Black
+                    )
+                }
+
+
+                Column(
+                    modifier = Modifier
+                        .padding(start = 12.dp)
+                ) {
+                    Text(
+                        text = "Hola Saul",
+                        fontWeight = FontWeight.ExtraBold,
+                        fontSize = 24.sp
+                    )
+                    Text(
+                        text = "Bienvenido",
+                        fontSize = 15.sp,
+                        color = Color.Gray
+                    )
+                }
+
+
+            }
+
+            //Menu hamburguesa
+            Icon(
+                imageVector = Icons.Default.Menu,
+                contentDescription = "Menu",
+                modifier = Modifier.size(30.dp),
+                tint = Color.Black
+            )
+        }
 
         //SummaryCards
         Row() { }
