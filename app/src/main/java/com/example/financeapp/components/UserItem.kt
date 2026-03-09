ffacfdd1f91d1d81a247af866132168c348d8c5d
SaulFnck <sauld5354@gmail.com>
2026-03-09 01:18:21 -0600

    User DataClass and component

--- app/src/main/java/com/example/financeapp/components/UserItem.kt
+++ app/src/main/java/com/example/financeapp/components/UserItem.kt
@@ -1,2 +1,100 @@
 package com.example.financeapp.components
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.financeapp.models.User
+import com.example.financeapp.models.summaryCards
+import com.example.financeapp.models.users
+import com.example.financeapp.ui.theme.FinanceAppTheme
+import com.example.financeapp.ui.theme.beish
+
+@Composable
+fun UserItem(user: User) {
+    //Welcome
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(15.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween
+
+    )
+    {
+        //Usuario
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+        )
+        {
+            //Icono
+            Box(
+                modifier = Modifier
+                    .size(60.dp)
+                    .clip(CircleShape)
+                    .background(beish),
+                contentAlignment = Alignment.Center
+            ) {
+                Icon(
+                    imageVector = Icons.Default.AccountCircle,
+                    contentDescription = "Profile Picture",
+                    modifier = Modifier.size(50.dp),
+                    tint = Color.Black
+                )
+            }
+
+            //Texto Usuario
+            Column(
+                modifier = Modifier
+                    .padding(start = 12.dp)
+            ) {
+                Text(
+                    text = "Hola ${user.name}",
+                    fontWeight = FontWeight.ExtraBold,
+                    fontSize = 24.sp
+                )
+                Text(
+                    text = "Bienvenido",
+                    fontSize = 15.sp,
+                    color = Color.Gray
+                )
+            }
+        }
+
+        //Menu hamburguesa
+        Icon(
+            imageVector = Icons.Default.Menu,
+            contentDescription = "Menu",
+            modifier = Modifier.size(30.dp),
+            tint = Color.Black
+        )
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun greetingpreview() {
+    FinanceAppTheme {
+       UserItem(users[1])
+    }
+}
--- app/src/main/java/com/example/financeapp/models/User.kt
+++ app/src/main/java/com/example/financeapp/models/User.kt
@@ -5,3 +5,9 @@ data class User(
     val name: String,
     val saldoActual: Double,
 )
+
+val users = listOf(
+    User(id = 1, name = "Saul", saldoActual = 1000.0),
+    User(id = 2, name = "Juan", saldoActual = 2000.0),
+    User(id = 3, name = "Maria", saldoActual = 3000.0),
+)
