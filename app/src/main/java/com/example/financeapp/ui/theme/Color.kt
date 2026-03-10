f5e043d95ade165fe5990237a03a3dc324312112
SaulFnck <sauld5354@gmail.com>
2026-03-10 16:18:44 -0600

    Set Colors

--- app/src/main/java/com/example/financeapp/ui/theme/Color.kt
+++ app/src/main/java/com/example/financeapp/ui/theme/Color.kt
@@ -2,18 +2,31 @@ package com.example.financeapp.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
+// Colores Base
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
-
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
 
-val beish: Color = Color(0xFFF5E7DE)
+// Colores Para Cards
+val LightGreen = Color(0xFFE7F4E9)
+val LightYellow = Color(0xFFF5E7DE)
+val LightPurple = Color(0xFFE7E0F4)
+val LightGray = Color(0xFFF1F3F4)
 
-val Gray: Color = Color(0xFFF1F3F4)
+// Nombres Semánticos para Componentes
+val GreenCard = LightGreen
+val YellowCard = LightYellow
+val PurpleCard = LightPurple
+val SurfaceGray = LightGray
 
-val GreenCard: Color = Color(0xFFE7F4E9)
-val YellowCard: Color = Color(0xFFF5E7DE)
-val PurpleCard: Color = Color(0xFFE7E0F4)
+val TextPrimary = Color(0xFF000000)
+val TextSecondary = Color(0xFF757575)
+val White = Color(0xFFFFFFFF)
+val Black = Color(0xFF000000)
+
+// Alias para compatibilidad
+val beish = LightYellow
+val Gray = LightGray
