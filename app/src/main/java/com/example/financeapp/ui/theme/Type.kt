a8b15df2c7dd5594805a69d1a4642464fe88450b
SaulFnck <sauld5354@gmail.com>
2026-03-10 22:55:34 -0600

    Configuracion de tipografia

--- app/src/main/java/com/example/financeapp/ui/theme/Type.kt
+++ app/src/main/java/com/example/financeapp/ui/theme/Type.kt
@@ -2,33 +2,38 @@ package com.example.financeapp.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.financeapp.R
 
-// Set of Material typography styles to start with
+val NotoSerif = FontFamily(
+    Font(R.font.notoserif_light, FontWeight.Light),
+    Font(R.font.notoserif_semicondensed_regular, FontWeight.Normal),
+    Font(R.font.notoserifbold, FontWeight.Bold)
+)
+
 val Typography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = NotoSerif,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
+    ),
     titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
+        fontFamily = NotoSerif,
+        fontWeight = FontWeight.Bold,
         fontSize = 22.sp,
         lineHeight = 28.sp,
         letterSpacing = 0.sp
     ),
     labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = NotoSerif,
         fontWeight = FontWeight.Medium,
         fontSize = 11.sp,
         lineHeight = 16.sp,
         letterSpacing = 0.5.sp
     )
-    */
 )
